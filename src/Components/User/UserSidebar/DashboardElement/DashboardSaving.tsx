c017c080852d78aa79d0c91232cbf67e5812d668
futurelegend1 <shenglin322@gmail.com>
2026-04-27 03:04:21 -0400

    Adjusted unused file to pass CD checks

--- src/Components/User/UserSidebar/DashboardElement/DashboardSaving.tsx
+++ src/Components/User/UserSidebar/DashboardElement/DashboardSaving.tsx
@@ -1,24 +1,24 @@
-type SavingGoal = {
-  id: string;
-  targetGoal: string;
-  targetAmount: number;
-  CurrentAmount: number;
-  expectedCompletionDate: Date;
-};
-function DashboardSaving({ savingGoals }: { savingGoals: SavingGoal[] }) {
-  const nearestGoal = savingGoals.sort(
-    (a, b) =>
-      a.expectedCompletionDate.getTime() - b.expectedCompletionDate.getTime(),
-  )[0];
+// type SavingGoal = {
+//   id: string;
+//   targetGoal: string;
+//   targetAmount: number;
+//   CurrentAmount: number;
+//   expectedCompletionDate: Date;
+// };
+// function DashboardSaving({ savingGoals }: { savingGoals: SavingGoal[] }) {
+//   const nearestGoal = savingGoals.sort(
+//     (a, b) =>
+//       a.expectedCompletionDate.getTime() - b.expectedCompletionDate.getTime(),
+//   )[0];
 
-  return (
-    <div className="bg-slate-700 rounded-2xl p-4">
-      <h2 className="text-white text-center text-2xl font-semibold mb-4 ">
-        Nearest savings goal
-      </h2>
-      <div className="mt-px h-px bg-white/60 mx-4"></div>
-    </div>
-  );
-}
+//   return (
+//     <div className="bg-slate-700 rounded-2xl p-4">
+//       <h2 className="text-white text-center text-2xl font-semibold mb-4 ">
+//         Nearest savings goal
+//       </h2>
+//       <div className="mt-px h-px bg-white/60 mx-4"></div>
+//     </div>
+//   );
+// }
 
-export default DashboardSaving;
+// export default DashboardSaving;
